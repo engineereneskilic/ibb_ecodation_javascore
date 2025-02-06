e0ffc317af4cac69216d18656bbe101321c13550
engineereneskilic <engineer.eneskilic@gmail.com>
2025-02-06 18:37:22 +0300

    dd

---
+++ src/main/java/org/eneskilic/_1_javatypes/_01_JavaTypes.java
@@ -0,0 +1,26 @@
+package org.eneskilic._1_javatypes;
+
+// class isimleri PascalCase göre yazılır
+// class isimleri fiil olarak kullanmayız (mak-mek)
+// Class : Java static class yoktur (inner class hariç)
+
+
+// Paket isimler : fiil olarak kullanmayız
+// paket isimleri küçük harfle yazılır.
+
+// single comment
+/*
+
+
+
+ */
+
+// ctrl + alt + l
+// psvm
+// sout
+// her biten kelime ; ile bitirmek zorundayız
+public class _01_JavaTypes {
+    public static void main(String[] args) {
+        System.out.println("selam");
+    }
+}
