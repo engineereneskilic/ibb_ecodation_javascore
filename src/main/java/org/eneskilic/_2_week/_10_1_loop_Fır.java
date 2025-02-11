378333a31230179d449bbb21c0585037da8a2949
engineereneskilic <engineer.eneskilic@gmail.com>
2025-02-11 19:01:04 +0300

    java math

---
+++ src/main/java/org/eneskilic/_2_week/_10_1_loop_Fır.java
@@ -0,0 +1,24 @@
+package org.eneskilic._2_week;
+
+public class _10_1_loop_Fır {
+
+    public static void main(String[] args) {
+
+        for (int i = 1 ; i < 10;i++ ) {
+            System.out.println(i+" - ");
+        }
+
+        int k = 0;
+        while(k <= 10) {
+            System.out.println(k  +  " -  ");
+            k++;
+        }
+
+
+
+
+
+    }
+
+
+}
